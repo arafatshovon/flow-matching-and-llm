a52907ac971a168c903f182602cff42a0df534a6
arafat_shovon <arafatshovon1897@gmail.com>
2025-11-26 15:47:04 +0600

    Added train.sh File

---
+++ train.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "$(pwd)"
+echo -e "activating env"
+source flow_env/bin/activate
+
+jupyter nbconvert --to notebook --execute notebooks/Flow_Matching.ipynb --ExecutePreprocessor.timeout=-1 --output Flow_Matching_output.ipynb 2>&1 | tee execution.log
+rm -rf notebooks/lightning_logs
+
+deactivate
+
+
