a51d22ae53f95a96609b8a9a4924a78cab28144c
arafat_shovon <arafatshovon1897@gmail.com>
2025-12-01 12:17:01 +0600

    Finishing Flow Matching

--- train.sh
+++ train.sh
@@ -5,7 +5,7 @@ echo -e "activating env"
 source flow_env/bin/activate
 
 jupyter nbconvert --to notebook --execute notebooks/Flow_Matching.ipynb --ExecutePreprocessor.timeout=-1 --output Flow_Matching_output.ipynb 2>&1 | tee execution.log
-rm -rf notebooks/lightning_logs
+# rm -rf notebooks/lightning_logs
 
 deactivate
 
